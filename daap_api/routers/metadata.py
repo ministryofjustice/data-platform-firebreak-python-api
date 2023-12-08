5588aacd2235ab2a4c1a0c301e1a4665ae99c424
Mat Moore <MatMoore@users.noreply.github.com>
2023-12-08 16:14:26 +0000

    Add get schemas endpoint

--- daap_api/routers/metadata.py
+++ daap_api/routers/metadata.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, HTTPException
 
-from ..config import Settings
 from ..services.data_platform_logging import DataPlatformLogger
-from ..services.data_product_metadata import DataProductMetadata
+from ..services.data_product_metadata import (
+    DataProductMetadata,
+    DataProductSchema,
+    format_table_schema,
+)
 
 router = APIRouter()
 
@@ -26,3 +29,25 @@ async def get_metadata(data_product_name: str):
         raise HTTPException(status_code=404, detail=message)
 
     return metadata.latest_version_saved_data
+
+
+@router.get("/schemas/{data_product_name}/{table_name}")
+async def get_metadata(data_product_name: str, table_name: str):
+    logger.add_data_product(data_product_name, table_name)
+
+    schema = DataProductSchema(
+        data_product_name=data_product_name,
+        table_name=table_name,
+        logger=logger,
+        input_data=None,
+    ).load()
+
+    if not schema.exists:
+        message = (
+            f"no existing schema found in S3 for {data_product_name=}, {table_name=}"
+        )
+        logger.error(message)
+        raise HTTPException(status_code=404, detail=message)
+
+    formatted = format_table_schema(schema.latest_version_saved_data)
+    return formatted
--- tests/integration/test_metadata_crud.py
+++ tests/integration/test_metadata_crud.py
@@ -28,3 +28,51 @@ def test_read_metadata():
             "table",
         ],
     }
+
+
+def test_read_schema():
+    response = client.get("/schemas/hmpps_use_of_force/statement")
+    assert response.status_code == 200
+    assert response.json() == {
+        "tableDescription": "",
+        "columns": [
+            {"name": "id", "type": "bigint", "description": ""},
+            {"name": "report_id", "type": "bigint", "description": ""},
+            {"name": "user_id", "type": "string", "description": ""},
+            {"name": "name", "type": "string", "description": ""},
+            {"name": "email", "type": "string", "description": ""},
+            {"name": "submitted_date", "type": "string", "description": ""},
+            {"name": "statement_status", "type": "string", "description": ""},
+            {"name": "last_training_month", "type": "string", "description": ""},
+            {"name": "last_training_year", "type": "string", "description": ""},
+            {"name": "job_start_year", "type": "string", "description": ""},
+            {"name": "statement", "type": "string", "description": ""},
+            {"name": "staff_id", "type": "bigint", "description": ""},
+            {"name": "created_date", "type": "timestamp", "description": ""},
+            {"name": "updated_date", "type": "string", "description": ""},
+            {"name": "next_reminder_date", "type": "string", "description": ""},
+            {"name": "overdue_date", "type": "string", "description": ""},
+            {"name": "in_progress", "type": "string", "description": ""},
+            {"name": "deleted", "type": "string", "description": ""},
+            {"name": "removal_requested_reason", "type": "string", "description": ""},
+            {"name": "removal_requested_date", "type": "string", "description": ""},
+        ],
+    }
+
+
+def test_missing_data_product():
+    response = client.get("/data-products/hmpps_use_of_the_force")
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "no existing metadata found in S3 for "
+        "data_product_name='hmpps_use_of_the_force'"
+    }
+
+
+def test_missing_schema():
+    response = client.get("/schemas/hmpps_use_of_force/missing")
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "no existing schema found in S3 for "
+        "data_product_name='hmpps_use_of_force', table_name='missing'"
+    }
