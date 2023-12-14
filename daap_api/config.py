4e1d781ca42180fc9bfa2d89a41654678739a69e
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-12-14 13:50:05 +0000

    add auth using fastapi-azure-auth get REDIS and DB URLs from kube secrets

--- daap_api/config.py
+++ daap_api/config.py
@@ -1,5 +1,7 @@
+import os
 from os import environ
 
+from pydantic import AnyHttpUrl, computed_field
 from pydantic_settings import BaseSettings
 
 
@@ -9,11 +11,38 @@ class Settings(BaseSettings):
     metadata_bucket_name: str = "metadata-development20231011135450514100000004"
     data_bucket_name: str = "data-development20231011135451999500000005"
     landing_zone_bucket_name: str = "data-landing-development20231011135449285800000001"
-    database_uri: str = "postgresql+psycopg://postgres:postgres123@localhost:5432/daap_api_dev"  # pragma: allowlist secret
+    database_uri: str = os.getenv(
+        key="DATABASE_URL",
+        default="postgresql+psycopg://postgres:postgres123@localhost:5432/daap_api_dev",  # pragma: allowlist secret
+    )
     database_uri_test: str = "postgresql+psycopg://postgres:postgres123@localhost:5432/daap_api_test"  # pragma: allowlist secret
+
+    BACKEND_CORS_ORIGINS: list[str | AnyHttpUrl] = ["http://localhost:8000"]
+    OPENAPI_CLIENT_ID: str = ""
+    APP_CLIENT_ID: str = ""
+    TENANT_ID: str = ""
+    SCOPE_DESCRIPTION: str = "user_impersonation"
+
+    @computed_field
+    @property
+    def SCOPE_NAME(self) -> str:
+        return f"api://{self.APP_CLIENT_ID}/{self.SCOPE_DESCRIPTION}"
+
+    @computed_field
+    @property
+    def SCOPES(self) -> dict:
+        return {
+            self.SCOPE_NAME: self.SCOPE_DESCRIPTION,
+        }
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
 
 
 settings = Settings()
+
 
 # Backwards compatability with old code
 environ["LOG_BUCKET"] = settings.log_bucket_name
--- daap_api/main.py
+++ daap_api/main.py
@@ -1,19 +1,48 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Security
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi_azure_auth import SingleTenantAzureAuthorizationCodeBearer
+from pydantic import AnyHttpUrl, computed_field
 
+from .config import settings
 from .db import create_db_and_tables
 from .routers import ingestion, metadata
 
 
 async def lifespan(app: FastAPI):
     create_db_and_tables()
+    await azure_scheme.openid_config.load_config()
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,  # type: ignore
+    swagger_ui_oauth2_redirect_url="/oauth2-redirect",
+    swagger_ui_init_oauth={
+        "usePkceWithAuthorizationCodeGrant": True,
+        "clientId": settings.OPENAPI_CLIENT_ID,
+    },
+)
 app.include_router(ingestion.router)
 app.include_router(metadata.router)
 
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-@app.get("/")
+
+azure_scheme = SingleTenantAzureAuthorizationCodeBearer(
+    app_client_id=settings.APP_CLIENT_ID,
+    tenant_id=settings.TENANT_ID,
+    scopes=settings.SCOPES,
+)
+
+
+@app.get("/", dependencies=[Security(azure_scheme)])
 async def root():
     return {"message": "Hello World"}
