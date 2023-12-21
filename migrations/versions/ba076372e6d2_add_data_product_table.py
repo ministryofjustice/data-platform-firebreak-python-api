82c39dc4c1b2db52ee51d231f9d168a2f0a6f2e0
Mat Moore <MatMoore@users.noreply.github.com>
2023-12-21 15:14:23 +0000

    Add migration for new table

---
+++ migrations/versions/ba076372e6d2_add_data_product_table.py
@@ -0,0 +1,33 @@
+"""Add data product table
+
+Revision ID: ba076372e6d2
+Revises: 89cac6c73079
+Create Date: 2023-12-21 15:08:42.133330
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "ba076372e6d2"  # pragma: allowlist secret
+down_revision: Union[str, None] = "89cac6c73079"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "data_products",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.Column("current_version_id", sa.Integer(), nullable=False),
+    )
+    op.create_foreign_key(
+        None, "data_products", "data_product_versions", ["current_version_id"], ["id"]
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("data_products")
